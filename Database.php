a08b55fd156e9047bf0049d3a4821180cdfbdf4e
isonekai <leanhtuan02072006@gmail.com>
2026-03-05 17:21:10 +0700

    update

--- Database.php
+++ Database.php
@@ -1,19 +1,29 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $dbname = "athletehub";
+    private $dbname = "shop_ethletehub"; 
     private $user = "root";
     private $pass = "";
+    private $port = "3307";
+
     public $conn;
+
     public function connect() {
         $this->conn = null;
+
         try {
-            // Chú ý dùng biến $this-> để lấy giá trị từ thuộc tính class
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=utf8", $this->user, $this->pass);
+            $this->conn = new PDO(
+                "mysql:host=".$this->host.";port=".$this->port.";dbname=".$this->dbname.";charset=utf8",
+                $this->user,
+                $this->pass
+            );
+
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         } catch (PDOException $e) {
             echo "Lỗi kết nối: " . $e->getMessage();
         }
+
         return $this->conn;
     }
 }
