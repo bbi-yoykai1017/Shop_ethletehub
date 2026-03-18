584ca138607553176e4df1d9a7a6536ebd4f2193
kaido <leanhtuan02072006@gmail.com>
2026-03-18 15:46:03 +0700

    them thanh navbar cho trang login

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ if (isset($_POST['login_btn'])) {
         $conn = $db->connect();
 
         // goi ham dang nhap tu fuction 
-        $result = loginUser($conn,$email,$password);
+        $result = loginUser($conn, $email, $password);
 
         if ($result['success']) {
             // luu thong tin cua 
@@ -38,74 +38,46 @@ if (isset($_POST['login_btn'])) {
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập - AthleteHub</title>
 
-    <title>login</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/variables.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
-    <div class="container mt-3 p-2 my-5 bg-dark text-white" style="text-align: center;">
-
-        <ul class="nav justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link" style="color: white;">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: white;">Đăng nhập</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" style="color: aqua;">Đăng ký</a>
-            </li>
-
-        </ul>
-    </div>
-    <div class="container" style="display: flex; justify-content: center; padding-top: 50px;">
-        <div id="loginbox" class="col-md-6 col-sm-8" style="border: 2px solid black ;">
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <div class="panel-title text-center mt-3">
-                        <h3>LOGIN</h3>
-                    </div>
-                </div>
-
-                <div style="padding: 30px" class="panel-body">
-
-                    <form method="post" class="form-horizontal">
-                        <?php if ($error): ?>
-
-                            <div class="alert alert-danger text-center"><?= $error ?></div>
-                        <?php endif; ?>
-
-                        <div class="input-group" style="margin-bottom: 20px">
-                            <span class="input-group-addon">Email</span>
-                            <input type="email" name="email" class="form-control" required>
-                        </div>
-
-                        <div class="input-group" style="margin-bottom: 20px">
-                            <span class="input-group-addon">Mật khẩu</span>
-                            <input type="password" name="mat_khau" class="form-control" required>
-                        </div>
-                        <div class="margin-bottom-25">
-                            <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-                            <label for="remember"> Ghi nhớ đăng nhập</label>
-                        </div>
-                        <div class="form-group text-center">
-                            <button type="submit" name="login_btn" class="btn btn-primary">Đăng nhập</button>
-                        </div>
-                    </form>
-                </div>
+ <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container-fluid px-4">
+            <a class="navbar-brand" href="index.php">
+                <i class="fas fa-dumbbell"></i> AthleteHub
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Trang chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="products.php">Sản phẩm</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
-    <footer>
-        <div class="container mt-3 p-3 my-5 bg-dark text-white" style="text-align: center;">
-            <span>Lập trình backend wed 2</span>
-        </div>
-    </footer>
-
+    </nav>
+    
 </body>
 
 </html>
