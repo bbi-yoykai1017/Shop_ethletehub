37978ac7ce1a675c28613aa50760f0352707c8c9
kaido <leanhtuan02072006@gmail.com>
2026-03-18 16:56:02 +0700

    đăng xuất oke, xóa tất cả session, cookie

--- logout.php
+++ logout.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
+ 
+// Xóa tất cả session
+$_SESSION = array();
+ 
+// Xóa session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+ 
+// Destroy session
 session_destroy();
+ 
+// Redirect to home page
 header("Location: index.php");
 exit();
+?>
+ 
