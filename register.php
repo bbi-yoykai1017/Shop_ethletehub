faa2a5b722ab17253df082d798065ca8d2241ef4
kaido <leanhtuan02072006@gmail.com>
2026-03-19 13:52:07 +0700

    kiem tra input, khoi tao db va goi ham dang ky tu function

--- register.php
+++ register.php
@@ -2,8 +2,6 @@
 session_start();
 require_once "Database.php";
 require_once "functions.php";
-$db = new Database();
-$conn = $db->connect();
 
 if (isset($_SESSION['user_id'])) {
     header('localhost: index.php');
@@ -18,18 +16,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $confirm_password = trim($_POST['comfirm_password'] ?? '');
 
     // kiem tra input 
-   
+    if (empty($ten) || empty($email) || empty($password) || empty($confirm_password)) {
+        $error_message = "Vui lòng nhập đầy đủ thông tin";
+    } elseif (strlen($password) < 6) {
+        $error_message = "Mật khẩu phải có trên 6 ký tự";
+    } elseif ($password != $confirm_password) {
+        $error_message = "Mật khẩu không trùng khớp";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error_message = "Email không hợp lệ";
+    } else {
+        // khoi tao db
+        $db = new Database();
+        $conn = $db->connect();
+
+        // goi ham dang ky 
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
