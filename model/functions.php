4ded724eb29a1cd0a1fcdef72240b53802e29c27
td130999-cell <td130999@gmail.com>
2026-03-23 11:31:43 +0700

    Cập nhập funtions.php  và cập nhập CRUDdonhang.php

--- model/functions.php
+++ model/functions.php
@@ -491,28 +491,28 @@ function loginUser($conn, $email, $mat_khau) {
 }
 // lay danh sach san pham
 function getAllProducts($conn) {
-    $sql = "SELECT * FROM san_pham WHERE trang_thai = 1 ORDER BY id ASC";
+    $sql = "SELECT * FROM san_pham ";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 } 
 // lay danh sach giam gia
 function getAllDiscounts($conn) {
-    $sql = "SELECT * FROM ma_giam_gia WHERE trang_thai = 1 ORDER BY id ASC";
+    $sql = "SELECT * FROM ma_giam_gia ";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 // lay bien the san pham
 function getAllVariants($conn) {
-    $sql = "SELECT * FROM bien_the_san_pham WHERE trang_thai = 1 ORDER BY id ASC";
+    $sql = "SELECT * FROM bien_the_san_pham ";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 // lay danh sach don hang
 function getAllOrders($conn) {
-    $sql = "SELECT * FROM don_hang WHERE trang_thai = 1 ORDER BY id ASC";
+    $sql = "SELECT * FROM don_hang  ";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
