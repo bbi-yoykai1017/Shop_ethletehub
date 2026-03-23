7a233b09b84cb250b31207c9cba4c365649685a5
td130999-cell <td130999@gmail.com>
2026-03-23 10:46:57 +0700

    Tao trang CRUDbienthesp.php va cap nhap funtions.php

--- model/functions.php
+++ model/functions.php
@@ -498,7 +498,14 @@ function getAllProducts($conn) {
 } 
 // lay danh sach giam gia
 function getAllDiscounts($conn) {
-    $sql = "SELECT * FROM giam_gia WHERE trang_thai = 1 ORDER BY id ASC";
+    $sql = "SELECT * FROM ma_giam_gia WHERE trang_thai = 1 ORDER BY id ASC";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+// lay bien the san pham
+function getAllVariants($conn) {
+    $sql = "SELECT * FROM bien_the_san_pham WHERE trang_thai = 1 ORDER BY id ASC";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
