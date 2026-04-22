3bfee9fefc10d6ff787d4249a817184e58a0c050
kaido <leanhtuan02072006@gmail.com>
2026-04-22 15:19:31 +0700

    tao key và thư viện

--- model/Mailer.php
+++ model/Mailer.php
@@ -2,8 +2,25 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception as PHPMailerException;
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+class Mailer
+{
+    // cau hinh smtp 
+    private string $smtpHost;
+    private int    $smtpPort;
+    private string $smtpUser;
+    private string $smtpPass;
+    private string $fromEmail = 'shopathletehub@gmail.com';
+    private string $fromName = 'AthleteHub';
+    private string $shopUrl = 'https://shopethletehub.kesug.com/';
 
-class Mailer {
-    
+     public function __construct()
+    {
+        $this->smtpHost = $_ENV['SMTP_HOST'];
+        $this->smtpPort = (int) $_ENV['SMTP_PORT'];
+        $this->smtpUser = $_ENV['SMTP_USER'];
+        $this->smtpPass = $_ENV['SMTP_PASS'];
+    }
 }
 ?>
